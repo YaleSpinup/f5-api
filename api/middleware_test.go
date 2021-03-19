414c1262e8f1bbbe4a5a6531af05b4db5da0145a
Darryl Wisneski <darryl.wisneski@yale.edu>
2021-03-19 09:00:15 -0400

    updated middleware_test.go

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright © 2021 Yale University
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package api
 
 import (
@@ -5,10 +21,13 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestTokenMiddleware(t *testing.T) {
-	psk := "sometesttoken"
+	psk := []byte("sometesttoken")
+	tokenHeader, _ := bcrypt.GenerateFromPassword(psk, bcrypt.DefaultCost)
 
 	// Test handler that just returns 200 OK
 	okHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -57,7 +76,7 @@ func TestTokenMiddleware(t *testing.T) {
 	// Test a private URL _with_ an auth-token
 	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/private", server.URL), nil)
-	req.Header.Add("X-Auth-Token", psk)
+	req.Header.Add("X-Auth-Token", string(tokenHeader))
 	resp, err = client.Do(req)
 	if err != nil {
 		t.Fatal(err)
